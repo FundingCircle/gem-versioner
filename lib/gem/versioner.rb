ea3379eecaa8e74d5ab98dd4aa2e7069227b3dbc
Antonis Berkakis <aberkakis@gmail.com>
2016-09-12 17:40:42 +0100

    first commit

---
+++ lib/gem/versioner.rb
@@ -0,0 +1,7 @@
+require "gem/versioner/version"
+
+module Gem
+  module Versioner
+    # Your code goes here...
+  end
+end
---
+++ spec/gem/versioner_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Gem::Versioner do
+  it 'has a version number' do
+    expect(Gem::Versioner::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
